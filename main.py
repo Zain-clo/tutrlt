58bda542afc8e3b5ecb08fac542dc260e2df93b9
Zain-clo <zainablawal2013ab@gmail.com>
2025-03-15 18:53:53 -0700

    turtle

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+sc=turtle.Screen()
+sc.bgcolor("red")
+sc.setup(300,300)
+sc.title("Background color")
+
+
+
+t = turtle.Turtle()
+for i in range(4):
+    t.forward(170)
+    t.left(90)
+turtle.done()
+ 
+
