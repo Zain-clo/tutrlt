4fe143214ebb78c40fe3cecf9a347d7282000e74
Zain-clo <zainablawal2013ab@gmail.com>
2025-03-22 18:58:48 -0700

    tuples

--- data typees/ass.py
+++ data typees/ass.py
@@ -7,24 +7,22 @@ def number_guessing_game():
     
     # Step 2: The computer randomly selects a number
     number_to_guess = random.randint(lower_bound, upper_bound)
-    
-    # Step 3: Set the initial number of attempts
+   
     attempts = 0
     
     # Step 4: Provide an introductory message
     print(f"Welcome to the Number Guessing Game!")
     print(f"I'm thinking of a number between {lower_bound} and {upper_bound}. Try to guess it!")
     
-    # Step 5: Start the game loop
+  
     while True:
-        # Step 6: Ask the player for a guess
+      
         try:
             guess = int(input("Enter your guess: "))
         except ValueError:
             print("Please enter a valid number!")
             continue
-        
-        # Step 7: Increase the number of attempts
+       
         attempts += 1
         
-        # Step 8: Check if the guess is correct, too
+      
---
+++ data typees/main-5.py
@@ -0,0 +1,8 @@
+my_tuple = ("Zainab", "mariam", "Soliat")
+print(my_tuple[-1])
+my_colors = {"green", "blue", "red"}
+ur_colors = {"yellow", "orange", "purple"}
+print(my_colors.union(ur_colors))
+print(my_colors.difference(ur_colors))
+print(my_colors.intersection(ur_colors))
+print(my_colors.symmetric_difference(ur_colors))
